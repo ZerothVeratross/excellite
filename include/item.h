329434893e9033bc18f1b9ce4d8a864ddd27ec40
Zeroth Veratross <120138654+ZerothVeratross@users.noreply.github.com>
2023-01-08 19:04:54 -0300

    TMs are stored as single bits, and are reusable
    TMs also can't be tossed, stored in the PC, sold or bought/picked up more than once. Quantity of TMs is no longer displayed

--- include/item.h
+++ include/item.h
@@ -33,6 +33,7 @@ struct BagPocket
 
 extern const struct Item gItems[];
 extern struct BagPocket gBagPockets[];
+extern struct ItemSlot gTmHmItemSlots[BAG_TMHM_COUNT];
 
 void ApplyNewEncryptionKeyToBagItems(u32 newKey);
 void ApplyNewEncryptionKeyToBagItems_(u32 newKey);
@@ -76,5 +77,6 @@ u8 ItemId_GetBattleUsage(u16 itemId);
 ItemUseFunc ItemId_GetBattleFunc(u16 itemId);
 u8 ItemId_GetSecondaryId(u16 itemId);
 u8 ItemId_GetFlingPower(u16 itemId);
+void DeserializeTmHmItemSlots(void);
 
 #endif // GUARD_ITEM_H
