af95c8280dcae892d42fe565c5afffabe45dcad6
Zeroth Veratross <120138654+ZerothVeratross@users.noreply.github.com>
2023-01-22 14:16:05 -0300

    Remove unused metatile behavior and merge

--- src/metatile_behavior.c
+++ src/metatile_behavior.c
@@ -359,14 +359,6 @@ bool8 MetatileBehavior_IsTrickHouseSlipperyFloor(u8 metatileBehavior)
         return FALSE;
 }
 
-bool8 Unref_MetatileBehavior_IsUnused05(u8 metatileBehavior)
-{
-    if (metatileBehavior == MB_UNUSED_05)
-        return TRUE;
-    else
-        return FALSE;
-}
-
 bool8 MetatileBehavior_IsWalkNorth(u8 metatileBehavior)
 {
     if (metatileBehavior == MB_WALK_NORTH)
